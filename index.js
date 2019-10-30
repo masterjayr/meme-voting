81f170bd09dd4b5bc0ff936f18acc5f9702a54f1
masterjayr <46963516+masterjayr@users.noreply.github.com>
2019-10-30 14:57:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -45,9 +45,11 @@ async function callStatic(func, args){
   const contract = await client.getContractInstance(contractSource, {contractAddress});
   const calledGet = await contract.call(func, args, {callStatic: true}).catch(e=>console.error(e));
   console.log('calledGet', calledGet);
-  const decodedGet = await calledGet.decode().catch(e=>console.error(e));
-  console.log('decoded get', decodedGet);
-  return decodedGet;
+  const decodedResult = calledGet.decodedResult;
+  return decodedResult;
+  // const decodedGet = await calledGet.decode().catch(e=>console.error(e));
+  // console.log('decoded get', decodedGet);
+  // return decodedGet;
 }
 
 window.addEventListener('load', async () => {
